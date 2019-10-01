16eab06ee8e8046cee149c263cbf97d6b392b59d
Zakru <sakari.leukkunen@gmail.com>
2019-10-01 16:58:16 +0300

    Add project files.

---
+++ RBGController/App.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Net;
+using System.IO.Ports;
+using System.Threading;
+
+namespace RBGController
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        public static HttpListener httpListener;
+        public static SerialPort serialPort;
+
+        private static App app;
+        private static List<string> previousPorts = new List<string>();
+
+        [STAThread]
+        public static void Main()
+        {
+            httpListener = new HttpListener();
+            httpListener.Prefixes.Add("http://localhost:8081/");
+            httpListener.Start();
+            HttpListenerLoop();
+
+            Console.WriteLine("asd");
+            foreach (string port in SerialPort.GetPortNames())
+            {
+                Console.WriteLine(port);
+            }
+            serialPort = new SerialPort();
+            //serialPort.PortName = "";
+            serialPort.BaudRate = 57600;
+
+            PortListenerLoop();
+
+            app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+
+        private static Task PortListenerLoop()
+        {
+            return Task.Run(() =>
+            {
+                while (true)
+                {
+                    string[] portNames = SerialPort.GetPortNames();
+
+                    if (portNames.Length != previousPorts.Count) UpdatePorts();
+                    else
+                    {
+                        for (int i = 0; i < portNames.Length; i++)
+                        {
+                            if (!portNames[i].Equals(previousPorts[i]))
+                            {
+                                UpdatePorts();
+                                break;
+                            }
+                        }
+                    }
+
+                    Thread.Sleep(100);
+                }
+            });
+        }
+
+        private static void UpdatePorts()
+        {
+            string[] ports = SerialPort.GetPortNames();
+            previousPorts.Clear();
+            previousPorts.AddRange(ports);
+            ((MainWindow)app.MainWindow).portSelectorItems.Clear();
+            foreach (string port in ports) ((MainWindow)app.MainWindow).portSelectorItems.Add(port);
+        }
+
+        private static async void HttpListenerLoop()
+        {
+            while (httpListener.IsListening)
+            {
+                try
+                {
+                    HttpListenerContext context = await httpListener.GetContextAsync();
+                    if (context.Request.HttpMethod == "POST")
+                    {
+                        context.Response.StatusCode = 200;
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = 400;
+                    }
+                    context.Response.Close();
+                }
+                catch
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ RBGController/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RBGController
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<string> portSelectorItems = new ObservableCollection<string>();
+
+        private readonly int[] bitrates = {
+            300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 28800, 38400, 57600, 115200
+        };
+
+        public ObservableCollection<string> framerateSelectorItems = new ObservableCollection<string>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Closed += MainWindow_Closed;
+            ((ComboBox)FindName("portSelector")).ItemsSource = portSelectorItems;
+            ((ComboBox)FindName("framerateSelector")).ItemsSource = framerateSelectorItems;
+        }
+
+        private void MainWindow_Closed(object sender, EventArgs e)
+        {
+            App.httpListener.Close();
+        }
+
+        private void portSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            App.serialPort.PortName = (string)e.AddedItems[0];
+        }
+
+        private void pixelCountBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+
+        private static readonly Regex _regex = new Regex("[^0-9]+");
+        private static bool IsTextAllowed(string text)
+        {
+            return !_regex.IsMatch(text);
+        }
+
+        private void pixelCountBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string text = ((TextBox)sender).Text;
+            int pixels = text.Equals("") ? 0 : int.Parse(text);
+            int bytesToSend = 3 * pixels + 1;
+            framerateSelectorItems.Clear();
+            foreach (int rate in bitrates)
+            {
+                framerateSelectorItems.Add(((float)rate / bytesToSend).ToString("N2"));
+            }
+        }
+    }
+}
